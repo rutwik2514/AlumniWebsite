d9ffe2170938ad64e7c4b388cfc651ebe76f13a8
unknown <rutwik2514@gmail.com>
2023-09-11 11:49:07 +0530

    login page made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from './Components/Homepage';
+import Login from './Components/Login/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/login" element={<Login />} />
+
         </Routes>
       </BrowserRouter>
   )
